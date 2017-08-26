f81ea134f9a546f08c14fd04b510675528de1abb
eqs <i11murasige@gmail.com>
2017-08-26 14:50:40 +0900

    cherry-pick 3

--- server/auto_post.py
+++ server/auto_post.py
@@ -8,11 +8,15 @@ Created on 08/26/17 14:03:55
 """
 
 import sys
+<<<<<<< HEAD
 import os
+=======
+>>>>>>> 72b4419... :fire: Post a pile of JSONs to server :fire:
 import requests
 import json
 import time
 
+<<<<<<< HEAD
 os.environ['NO_PROXY'] = 'localhost'
 
 url = 'http://localhost:3001/api/color'
@@ -27,3 +31,13 @@ for k in range(50):
     res = requests.post(url, data=json.dumps({'colors' : ['#7070E0']}), headers=headers)
     print('{0:3d} {1}'.format(k, res))
     time.sleep(3)
+=======
+url = 'http://localhost:3001/api/color'
+data = {'colors' : ['#FF0000', '#00FF00', '#0000FF', '#000000']}
+headers = {'Accept' : 'application/json', 'Content-type' : 'application/json'}
+
+for k in range(100):
+    res = requests.post(url, data=json.dumps(data), headers=headers)
+    print('{0:3d} {1}'.format(k, res))
+    time.sleep(0.1)
+>>>>>>> 72b4419... :fire: Post a pile of JSONs to server :fire:
